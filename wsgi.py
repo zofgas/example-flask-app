ca5b2eadff056854ad66dfa597e37ae2ebf503a8
Oleksandr Fedoruk <oleksandr.fedoruk@hotmail.com>
2022-11-15 21:32:13 +0100

    Add auto database creation

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,10 @@
+import os
+from app.database import init_db
 from app.main import app
- 
+
+
 if __name__ == "__main__":
+    if not os.path.isfile("./test.db"):
+        init_db()
     app.run()
     
